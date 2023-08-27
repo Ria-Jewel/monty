69f730f97d29f0fd67b0065978d8a318d335b890
Gloria John <glogloriajohn@gmail.com>
2023-08-27 21:20:22 +0000

    FIFO, LIFO

--- monty.c
+++ monty.c
@@ -1,4 +1,4 @@
-#include <monty.h>
+#include "monty.h"
 
 int main(int argc, char *argv[])
 {
@@ -19,11 +19,37 @@ if (file == NULL)
 
 {
 
-fprintf(stderr, "Error: Can't open file ");
+fprintf(stderr, "Error: Can't open file %s\n", filepath);
 exit(EXIT_FAILURE);
 
 }
 
+char *buffer_line = NULL;
+size_t len = 0;
+int line_number = 0;
+
+while (getline(&buffer_line, &len, file) != -1)
+
+{
+
+line_number++;
+char *opcode = strtok(&buffer_line, "\t\n");
+if (opcode == NULL || strlen(opcode) == 0)
+
+{
+
+continue;
+
+}
+
+fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+exit(EXIT_FAILURE);
+
+}
+
+free(buffer_line);
+fclose(file);
+exit(EXIT_FAILURE);
 return(0);
 }
 
